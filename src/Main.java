c1402c39742ab8c4e860d231559dc0c894a651d8
Ivan Torres <ivan.look3002@gmail.com>
2024-12-15 04:14:00 -0500

    aa

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import views.*;
 
 public class Main {
     private static Registro baseDatos;
+    public static final String ROLUSERADMIN = "admin";
+    public static final String ROLUSERCLIENTE = "client";
+    public static final String ROLUSEREMPLEADO = "emp";
     
     private static void adminView (Usuario user) {
         baseDatos.form.Frame.setVisible(false);
@@ -13,13 +16,40 @@ public class Main {
         });
     }
 
-    public static void main(String[] args) {
-        Registro registro = new Registro(user -> {
-            if (user.rol.equals("admin")) {
-                adminView(user);
+    private static void empleadoView (Usuario user) {
+        baseDatos.form.Frame.setVisible(false);
+        Empleado frame = new Empleado(e -> {
+            if (e.equals("cerrar")) {
+                baseDatos.form.Frame.setVisible(true);
             }
         });
-        registro.addUsuario(new Usuario("Pablo", "12345", "admin", 0));
+    }
+
+    private static void mainStoreView (Usuario user) {
+        baseDatos.form.Frame.setVisible(false);
+        MainStoreView mainStore = new MainStoreView(e -> {
+            if (e.equals("cerrar")) {
+                baseDatos.form.Frame.setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Registro registro = new Registro(user -> {
+            switch (user.rol) {
+                case "admin":
+                    adminView(user);
+                    break;
+                case "emp":
+                    empleadoView(user);
+                    break;
+                default:
+                    mainStoreView(user);
+                    break;
+            }
+        });
+        registro.addUsuario(new Usuario("Eduin", "12345", ROLUSERADMIN, 0));
+        registro.addUsuario(new Usuario("Sara", "12345", ROLUSEREMPLEADO, 0));
         baseDatos = registro;
     }
 
